efcfab7f18d4d6e5681413a683325cf00fe40b8b
Marushini <marushinibabu2203@gmail.com>
2024-12-19 23:39:16 +0530

    done

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-// server.js
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
-// Load environment variables
 dotenv.config();
-
 const app = express();
 app.use(express.json());
 
@@ -19,8 +16,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/replies', replyRoutes);
 
-// Connect to Database and Start Server
-connectDB();
+// Default 404 Handler
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
 
+// Connect to MongoDB and Start Server
+connectDB();
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
